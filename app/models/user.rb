c33a8a06bb25b77f025c308f14c3b3029972eeec
NataliaColome <nmcolome@gmail.com>
2020-02-05 16:56:40 -0700

    Changing email uniqueness validation to case  insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, email: true, confirmation: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, email: true, confirmation: true
   validates :password, presence: true
+
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User do
     it { should validate_presence_of :name }
     it { should validate_length_of(:name).is_at_most(50) }
     it { should validate_presence_of :email }
-    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_length_of(:email).is_at_most(255) }
     it { should validate_confirmation_of :email }
     it { should validate_presence_of :password }
