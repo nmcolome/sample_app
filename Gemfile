7048200b2917bcc13039342f533c660be7fc20c6
NataliaColome <nmcolome@gmail.com>
2020-02-05 20:33:37 -0700

    Add faker & factory_bot_rails, create user factory

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ group :development, :test do
   gem 'shoulda-matchers'
   gem 'capybara'
   gem 'launchy'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -60,6 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   config.filter_gems_from_backtrace('rack-2.1.2', 'rack-test-1.1.0', 'capybara-3.31.0', 'railties-5.1.7', 'shoulda-matchers-4.2.0')
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
