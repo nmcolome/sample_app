0a056aaa687fea89243c41829ee0e7ed43ee9bed
NataliaColome <nmcolome@gmail.com>
2020-02-06 15:35:32 -0700

    Refactor to #log_in & #current_user in SessionHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
+      log_in(user)
       redirect_to user_path(user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
