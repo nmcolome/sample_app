9eb0c0760d5f4c702654e74cbd731c4fdbbe3c82
NataliaColome <nmcolome@gmail.com>
2020-02-06 20:41:40 -0700

    Add log out route, #destroy action & helper

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
